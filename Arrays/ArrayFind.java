25db9d40ff0d42c94c03f6f4eb053a8ce1032da9
srikar-eranky <srikar.eranky@gmail.com>
2022-07-17 15:45:22 -0700

    Implemented the methods

--- Arrays/ArrayFind.java
+++ Arrays/ArrayFind.java
@@ -14,16 +14,58 @@ public class ArrayFind {
         System.out.println("###findNthElement. Enter n:");
         int[] a = {4,8,33,45,17,2,6,88};
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        String n = in.next();
         System.out.println("nth index = " + n);
 
-        // check if n is a valid value before picking the "n"th element 
-        // from array and printing it out
+        // check if array index is valid
+        if(validateArrayIndex(n, a.length)){
+            // print the value at the array index
+            int idx = Integer.parseInt(n);
+            if(idx == 0){
+                System.out.println(a[idx]);
+            } else {
+                System.out.println(a[idx-1]);
+            }
+        }else{
+            // print error message
+            System.out.println("Enter a valid index");
+        }
+
+    }
+
+    private static boolean validateInt(String input) {
+        boolean ret = false;
+        try { 
+			Integer.parseInt(input); 
+			System.out.println(input + " is a valid integer"); 
+            ret = true;
+		} catch (NumberFormatException e)  { 
+			System.out.println(input + " is not a valid integer"); 
+		} 
+        return ret;
+    }
+
+    private static boolean validateArrayIndex(String input, int length) {
+        boolean isValid = false;
+        if(validateInt(input)){
+            int idx = Integer.parseInt(input);
+            if((idx <= length) & (idx >= 0)){
+                isValid = true;
+            }
+        }
+        return isValid;
     }
 
     private static void findFirstOdd(){
         System.out.println("###findFirstOdd");
         int[] a = {4,8,33,45,17,2,6,88};
+        for(int i = 0; i < a.length; i++){
+            if((a[i]%2)==1){
+                System.out.println(a[i]);
+                System.out.println(i+1);
+                break;
+            }
+        }
         // print the first odd number and its index
     }
 
--- Arrays/ArraySum.java
+++ Arrays/ArraySum.java
@@ -22,8 +22,19 @@ public class ArraySum{
         System.out.println("###arraySumOddAndEven");
         int[] a = {3,5,2,7,88,12,6,99};
         // add the values of odd numbers separately and even numbers separately
-        // and print both the sums in 2 separate lines 
+        // and print both the sums in 2 separate lines
+        int evenSum = 0;
+        int oddSum = 0;
+        for(int i : a){
+            if((i%2) == 0){
+                evenSum += i;
+            } else {
+                oddSum += i;
+            }
+        }
 
+        System.out.println(evenSum);
+        System.out.println(oddSum);
     }
 
     private static void addArrays(){
@@ -31,7 +42,15 @@ public class ArraySum{
         int[] a1 = {1,2,3,4,5};
         int[] a2 = {6,7,8,9,10};
         // add the elements in same position in each array and print the new array
+        int[] sums = new int[a1.length];
+        for(int i = 0; i < a1.length; i++){
+            int sum = a1[i] + a2[i];
+            sums[i] = sum;
+        }
 
+        for(int j : sums){
+            System.out.print(j + " ");
+        }
 
     }
 
