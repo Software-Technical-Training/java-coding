1b17435127874ea8bed8450f78c86a829d071d22
Shyam Eranky <veranky@ebay.com>
2022-07-17 15:33:16 -0700

    New problems

---
+++ Arrays/ArrayMisc.java
@@ -0,0 +1,36 @@
+package Arrays;
+
+public class ArrayMisc {
+    
+    public static void main(String[] args) {
+        reverseArray();
+        rotateArray();
+        largestNum();
+        
+    }
+
+    private static void reverseArray() {
+        int[] a = {4,8,33,45,17,2,6,88};
+        // reverse the elements of the array and print it out
+        
+    }
+
+    private static void rotateArray() {
+        int[] a = {4,8,33,45,17,2,6,88};
+        // rotate this array counter clockwise by 4 elements and print
+        
+    }
+
+    private static void largestNum() {
+        int[] a = {4,8,33,45,17,2,6,88};
+        // arrange the elements in descending order and print
+        
+    }
+
+    private static void findMissingNum() {
+        int a = {1,2,3,4,6};
+        // print the missing num in the sequence 
+        // ex 5 in the above array
+        
+    }
+}
