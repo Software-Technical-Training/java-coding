b231b0ea34751353e3030dec17128cf99eeca099
srikar-eranky <srikar.eranky@gmail.com>
2022-07-29 15:05:45 -0700

    implemented runningSums

--- Arrays/ArrayMisc.java
+++ Arrays/ArrayMisc.java
@@ -161,7 +161,16 @@ Explanation: The array ans is formed as follows:
 Output: [1,3,6,10]
 Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
          */
-        
+        int[] nums = {1,2,3,4};
+        int[] newArr = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            int sum = 0;
+            for(int j = 0; j <= i; j++){
+                sum += nums[j];
+            }
+            newArr[i] = sum;
+        }
+        printArray(newArr);
     }
 
     private static void maxWealth() {
